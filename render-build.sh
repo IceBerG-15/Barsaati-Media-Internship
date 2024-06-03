51de137a4db562da63c74750d9dbd84f8f1c677d
Sidhant Sharma <warmachiness13@gmail.com>
2024-06-04 03:52:41 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,14 +3,17 @@
 # Install Google Chrome
 echo "Installing Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
+dpkg -x google-chrome-stable_current_amd64.deb /tmp/google-chrome
+cp /tmp/google-chrome/opt/google/chrome/chrome /usr/local/bin/
 
 # Install Chromedriver
 echo "Installing Chromedriver"
-CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
+CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P /tmp/
+unzip /tmp/chromedriver_linux64.zip -d /tmp/
+cp /tmp/chromedriver /usr/local/bin/
+chmod 0755 /usr/local/bin/chromedriver
+
+# Clean up
+rm -rf /tmp/google-chrome
+rm /tmp/chromedriver_linux64.zip
