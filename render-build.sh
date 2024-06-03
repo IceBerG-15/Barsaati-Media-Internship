45c65b79af0f80ac5762d5da687876e33362dd4f
Sidhant Sharma <warmachiness13@gmail.com>
2024-06-04 04:45:14 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,19 +3,21 @@
 # Install Google Chrome
 echo "Installing Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -x google-chrome-stable_current_amd64.deb /tmp/google-chrome
-mkdir -p /opt/google/chrome
-cp -r /tmp/google-chrome/opt/google/chrome/* /opt/google/chrome/
-ln -s /opt/google/chrome/google-chrome /usr/local/bin/google-chrome
+mkdir -p /usr/local/bin/chrome
+dpkg -x google-chrome-stable_current_amd64.deb /usr/local/bin/chrome
+chmod +x /usr/local/bin/chrome/opt/google/chrome/chrome
 
 # Install Chromedriver
 echo "Installing Chromedriver"
 CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P /tmp/
 unzip /tmp/chromedriver_linux64.zip -d /tmp/
-cp /tmp/chromedriver /usr/local/bin/
-chmod 0755 /usr/local/bin/chromedriver
+mv /tmp/chromedriver /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
+
+# Update PATH environment variable
+export PATH=$PATH:/usr/local/bin/chrome/opt/google/chrome/
 
 # Clean up
-rm -rf /tmp/google-chrome
+rm google-chrome-stable_current_amd64.deb
 rm /tmp/chromedriver_linux64.zip
